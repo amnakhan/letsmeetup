608d32f4e05cb1d521682b061f6465cd3817fe13
Claudius Mbemba <mbembac@gmail.com>
2014-10-17 11:49:51 -0400

    added lifecycle loggin

--- app/src/main/java/com/mbembac/letsmeetup/LoginActivty.java
+++ app/src/main/java/com/mbembac/letsmeetup/LoginActivty.java
@@ -3,6 +3,7 @@ package com.mbembac.letsmeetup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,10 +11,15 @@ import android.view.View;
 
 public class LoginActivty extends ActionBarActivity {
 
+    private final String TAG = ((Object) this).getClass().getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activty);
+
+        Log.e(TAG, "+++ In onCreate() +++");
+
 
     }
 
@@ -41,5 +47,52 @@ public class LoginActivty extends ActionBarActivity {
     public void login(View view) {
         Intent intent = new Intent(this, CloseFriendsActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.e(TAG, "+++ In onStart() +++");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.e(TAG, "+++ In onResume() +++");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.e(TAG, "+++ In onPause() +++");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.e(TAG, "+++ In onDestroy() +++");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.e(TAG, "+++ In onStop() +++");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.e(TAG, "+++ In onRestart() +++");
     }
 }
