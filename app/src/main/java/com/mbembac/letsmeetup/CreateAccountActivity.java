d8ab3540a3b18adea3ef74dd4fcfe8e58a6d6da8
Amna Khan <khan.261@osu.edu>
2014-10-17 18:26:39 -0400

    database working

--- app/src/main/java/com/mbembac/letsmeetup/CreateAccountActivity.java
+++ app/src/main/java/com/mbembac/letsmeetup/CreateAccountActivity.java
@@ -1,36 +1,97 @@
 package com.mbembac.letsmeetup;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
-public class CreateAccountActivity extends ActionBarActivity {
+public class CreateAccountActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    Button signup;
+    String usernametxt;
+    String passwordtxt;
+    EditText password;
+    EditText username;
+
+    String first_nametxt;
+    String last_nametxt;
+    String emailtxt;
+
+    EditText first_name;
+    EditText last_name;
+    EditText email;
+
+    /**
+     * Called when the activity is first created.
+     */
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get the view from main.xml
         setContentView(R.layout.activity_create_account);
-    }
+
+        // Locate EditTexts in main.xml
+        first_name = (EditText) findViewById(R.id.login_first_name);
+        last_name = (EditText) findViewById(R.id.login_last_name);
+        email = (EditText) findViewById(R.id.create_email);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        // Locate Buttons in main.xml
+        signup = (Button) findViewById(R.id.signup);
+
+        // Login Button Click Listener
+        signup.setOnClickListener(new OnClickListener() {
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.create_account, menu);
-        return true;
-    }
+            public void onClick(View arg0) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+                // Retrieve the text entered from the EditText
+                first_nametxt = first_name.getText().toString();
+                last_nametxt = last_name.getText().toString();
+                emailtxt = email.getText().toString();
+                usernametxt = username.getText().toString();
+                passwordtxt = password.getText().toString();
+
+                // Force user to fill up the form
+                if (usernametxt.equals("") && passwordtxt.equals("") && emailtxt.equals("")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Please fill in the username and password field.",
+                            Toast.LENGTH_LONG).show();
+
+                } else {
+                    // Save new user data into Parse.com Data Storage
+                    ParseUser user = new ParseUser();
+                    user.setEmail(emailtxt);
+                    user.setUsername(usernametxt);
+                    user.setPassword(passwordtxt);
+                    user.signUpInBackground(new SignUpCallback() {
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                // Show a simple Toast message upon successful registration
+                                Toast.makeText(getApplicationContext(),
+                                        "Successfully Registered!",
+                                        Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        "Sign up Error", Toast.LENGTH_LONG)
+                                        .show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/mbembac/letsmeetup/MainActivity.java
+++ app/src/main/java/com/mbembac/letsmeetup/MainActivity.java
@@ -1,36 +1,44 @@
 package com.mbembac.letsmeetup;
 
-import android.support.v7.app.ActionBarActivity;
+/**
+ * Created by amnakhan on 10/17/14.
+ */
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseUser;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        // Determine whether the current user is an anonymous user
+        if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
+            // If user is anonymous, send the user to LoginSignupActivity.class
+            Intent intent = new Intent(MainActivity.this,
+                    LoginSignupActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // If current user is NOT anonymous user
+            // Get current user data from Parse.com
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            if (currentUser != null) {
+                // Send logged in users to Welcome.class
+                Intent intent = new Intent(MainActivity.this, Welcome.class);
+                startActivity(intent);
+                finish();
+            } else {
+                // Send user to LoginSignupActivity.class
+                Intent intent = new Intent(MainActivity.this,
+                        LoginSignupActivity.class);
+                startActivity(intent);
+                finish();
+            }
         }
-        return super.onOptionsItemSelected(item);
+
     }
 }
