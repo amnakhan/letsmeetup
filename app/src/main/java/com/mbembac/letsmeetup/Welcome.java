6a4fec59a2c1e24945c33322a25ac3fc3a11b1c2
Claudius Mbemba <mbembac@gmail.com>
2014-10-22 09:33:48 -0400

    not yet added ignore files

--- app/src/main/java/com/mbembac/letsmeetup/Welcome.java
+++ app/src/main/java/com/mbembac/letsmeetup/Welcome.java
@@ -4,15 +4,16 @@ package com.mbembac.letsmeetup;
  * Created by amnakhan on 10/16/14.
  */
 
-import com.parse.ParseUser;
-
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Debug;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 public class Welcome extends Activity {
 
@@ -24,6 +25,8 @@ public class Welcome extends Activity {
         super.onCreate(savedInstanceState);
         // Get the view from singleitemview.xml
         setContentView(R.layout.welcome);
+
+        Debug.startMethodTracing();
 
         // Retrieve current user from Parse.com
         ParseUser currentUser = ParseUser.getCurrentUser();
@@ -50,5 +53,37 @@ public class Welcome extends Activity {
                 finish();
             }
         });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Debug.stopMethodTracing();
     }
 }
